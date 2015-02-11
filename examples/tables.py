8475367a9f43321696e7bae3756d157746ec918c
nypgand1 <nypgand1@gmail.com>
2015-02-11 13:57:22 +0800

    add missing column '_etag' for CommonColumns in examples

--- examples/tables.py
+++ examples/tables.py
@@ -23,6 +23,7 @@ class CommonColumns(Base):
     __abstract__ = True
     _created = Column(DateTime, default=func.now())
     _updated = Column(DateTime, default=func.now(), onupdate=func.now())
+    _etag = Column(String(40))
 
     @hybrid_property
     def _id(self):
