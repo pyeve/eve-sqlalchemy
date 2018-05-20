f0c17e2ebec6c59f6ccc948cba479986f31b05b0
Dominik Kellner <dkellner@dkellner.de>
2018-05-20 22:04:09 +0700

    Fix case of missing ETAG in response

--- eve_sqlalchemy/utils.py
+++ eve_sqlalchemy/utils.py
@@ -83,6 +83,10 @@ def sqla_object_to_dict(obj, fields):
             # (may be wrong embedding parameter)
             pass
 
+    # We have to remove the ETAG if it's None so Eve will add it later again.
+    if result.get(config.ETAG, False) is None:
+        del(result[config.ETAG])
+
     return result
 
 
